edfaa36eafd33efd9ead6870a5a5eb3294c2ac20
JMMS-HallPass <jaf2jc@bearworks.jackson.sparcc.org>
2026-08-28 22:55:59 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+
 import React, { useEffect, useMemo, useState } from 'react';
 import { AuthProvider, useAuth } from './contexts/AuthContext';
 import { Header } from './components/Header';
